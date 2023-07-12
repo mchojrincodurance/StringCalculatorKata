ff65678a6369b5552eef35725bfbf710d3058b67
Mauro Chojrin <mauro.chojrin@codurance.com>
2023-07-12 16:46:35 +0200

    Refactoring: collapsing all tests into a signle one

--- src/test/java/StringCalculatorShould.java
+++ src/test/java/StringCalculatorShould.java
@@ -8,26 +8,16 @@ public class StringCalculatorShould {
 
     private final StringCalculator stringCalculator = new StringCalculator();
 
-    @Test
-    public void return_0_when_the_input_is_empty() {
-        assertEquals("0", stringCalculator.add(""));
-    }
-
     @ParameterizedTest
     @CsvSource({
-            "1",
-            "2",
-    })
-    public void return_the_same_number_if_no_commas_are_in_the_input(String input) {
-        assertEquals(input, stringCalculator.add(input));
-    }
-
-    @ParameterizedTest
-    @CsvSource({
+            "'',0",
+            "'1',1",
+            "'2',2",
             "'0,1',1",
             "'1,2',3",
     })
-    public void return_sum_of_numbers_in_the_input_when_comma_is_present(String input, String result) {
+    public void calculate_the_sum_of_the_input(String input, String result)
+    {
         assertEquals(result, stringCalculator.add(input));
     }
 }
