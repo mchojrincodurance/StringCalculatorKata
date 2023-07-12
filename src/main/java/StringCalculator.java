3fa8372a7112f97cec2f47b540170ddaaef90a5d
Mauro Chojrin <mauro.chojrin@codurance.com>
2023-07-12 16:42:46 +0200

    Refactoring: extract magic constant

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,18 +1,21 @@
 import java.util.Arrays;
 
 public class StringCalculator {
+
+    public static final String SEPARATOR = ",";
+
     public String add(String s) {
         if (s.isEmpty()) {
 
             return "0";
         }
 
-        if (!s.contains(",")) {
+        if (!s.contains(SEPARATOR)) {
 
             return s;
         }
 
-        String[] parts = s.split(",");
+        String[] parts = s.split(SEPARATOR);
         int sum = Integer.parseInt(parts[0]) + Integer.parseInt(parts[1]);
 
         return Integer.toString(sum);
