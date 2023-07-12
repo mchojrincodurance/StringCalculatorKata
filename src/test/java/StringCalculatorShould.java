afe86cbdcd03119ebb19b151fb19413ba3aabf5c
Mauro Chojrin <mauro.chojrin@codurance.com>
2023-07-12 16:26:22 +0200

    Failing test for single digit input

--- src/test/java/StringCalculatorShould.java
+++ src/test/java/StringCalculatorShould.java
@@ -1,11 +1,23 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StringCalculatorShould {
+
+    private final StringCalculator stringCalculator = new StringCalculator();
+
     @Test
     public void return_0_when_the_input_is_empty() {
-        StringCalculator stringCalculator = new StringCalculator();
         assertEquals("0", stringCalculator.add(""));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "1",
+    })
+    public void return_the_same_number_if_no_commas_are_in_the_input(String input) {
+        assertEquals(input, stringCalculator.add(input));
     }
 }
